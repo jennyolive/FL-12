4e15d75c2e1fc07cff72cf2b0c004d261380cb61
Jenny <eugeniamaslina@gmail.com>
2019-12-24 17:00:22 +0200

    ready

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -2,6 +2,14 @@ let a = prompt('enter first side length');
 let b = prompt('enter second side length');
 let c = prompt('enter third side length');
 
-if (a <= 0 || b <= 0 || c <= 0) {
-    confirm('A triangle must have 3 sides with a positive definite length');
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    confirm('input values should be ONLY numbers'); 
+} else if (a <= 0 || b <= 0 || c <= 0) {
+     confirm('a triangle must have 3 sides with a positive definite length');
+} else if (a === b && b === c) {
+    confirm('equivalent triangle');
+} else if (a === b || b === c || c === a) {
+    confirm('Isosceles triangle');
+} else {
+    confirm("Triangle doesn't exist")
 }
