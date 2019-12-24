7a0bddb053bb2b185a2d3bc60a169e7bd8d2cd25
Jenny <eugeniamaslina@gmail.com>
2019-12-24 16:44:08 +0200

    add task2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,7 @@
-// Your code goes here
+let a = prompt('enter first side length');
+let b = prompt('enter second side length');
+let c = prompt('enter third side length');
+
+if (a <= 0 || b <= 0 || c <= 0) {
+    confirm('A triangle must have 3 sides with a positive definite length');
+}
