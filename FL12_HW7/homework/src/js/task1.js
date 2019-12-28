2d02ad910eeb6cfe4c2be6cd480cf32733096974
Jenny <eugeniamaslina@gmail.com>
2019-12-28 18:45:35 +0200

    add 1.1

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let email = prompt('Enter your email', '');
+let emailUser = 'user@gmail.com';
+let emailAdmin = 'admin@gmail.com';
+const maxmeilLength = 5;
+
+if(email === 0 || email === null) {
+    alert('Canceled');
+} else if (email.length < maxmeilLength){
+    alert('I dont know any emails having name length less than 5 symbols');
+} else if (email === emailUser || email === emailAdmin) {
+    let password = prompt('Enter your password', '');
+} else if 
+   (alert('I don’t know you')) ;
