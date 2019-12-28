3bb0161775694774fad661cd9a0e9d711f36ac96
Jenny <eugeniamaslina@gmail.com>
2019-12-28 19:04:20 +0200

    add 1.2

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,13 +1,22 @@
 let email = prompt('Enter your email', '');
 let emailUser = 'user@gmail.com';
 let emailAdmin = 'admin@gmail.com';
+let passwordUser = 'UserPass';
+let passwordAdmin = 'AdminPass';
 const maxmeilLength = 5;
 
-if(email === 0 || email === null) {
+if(email === '' || email === null) {
     alert('Canceled');
 } else if (email.length < maxmeilLength){
     alert('I dont know any emails having name length less than 5 symbols');
 } else if (email === emailUser || email === emailAdmin) {
     let password = prompt('Enter your password', '');
+    if (password === '' || password === null){
+        alert ('Canceled');
+    }
+    else if (password === passwordUser || password === passwordAdmin)
+    alert ('Success');
+    else 
+    alert('Wrong password');
 } else if 
    (alert('I don’t know you')) ;
