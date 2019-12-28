48589ec75f89e2b9d38de152a02674205b9cd3af
Jenny <eugeniamaslina@gmail.com>
2019-12-28 22:13:57 +0200

    add task2

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,7 @@
-// Your code goes here
+let gameStart = confirm('Do you want to play a game?', '');
+
+if (gameStart === true) {
+    alert('Lets play');
+} else {
+    alert('You did not become a billionaire, but can.');
+}
