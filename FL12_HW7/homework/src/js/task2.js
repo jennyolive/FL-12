b2de4502c0982a97fb84497953c9cb134b786ac0
Jenny <eugeniamaslina@gmail.com>
2019-12-29 10:58:04 +0200

    fixed

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,7 +1,11 @@
 let gameStart = confirm('Do you want to play a game?', '');
+let number;
+let range = 9;
+let attempts = 3;
 
 if (gameStart === true) {
-    alert('Lets play');
+    number = Math.floor(Math.random() * range);
+    
 } else {
     alert('You did not become a billionaire, but can.');
 }
