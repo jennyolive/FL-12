e4435ae646df999f74b4072f1eeece0cdf2c740e
Jenny <eugeniamaslina@gmail.com>
2019-12-29 20:38:49 +0200

    add task2

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,11 +1,41 @@
-let gameStart = confirm('Do you want to play a game?', '');
-let number;
-let range = 9;
-let attempts = 3;
-
-if (gameStart === true) {
-    number = Math.floor(Math.random() * range);
-    
-} else {
+let gameStart = confirm('Do you want to play a game?');
+if (!gameStart) {
     alert('You did not become a billionaire, but can.');
+}
+while (gameStart) {
+    let attempt;
+    let maxAttempt = 3;
+    let minAttempt = 1;
+    let maxPrize = 100;
+    let totalPrize = 0;
+    let prizeCoef = {
+        1: 1,
+        2: 2,
+        3: 4
+    };
+    let two = 2;
+    let maxNumber = 8;
+    let four = 4;
+    let nextGame;
+    for (attempt = minAttempt; attempt <= maxAttempt;) {
+        let randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+        console.log(randomNumber);
+        let inputNumber = prompt(`Choose a roulette pocket number from 0 to ${maxNumber}
+        Attempts left: ${maxAttempt+minAttempt-attempt}
+        Total prize: ${totalPrize}$
+        Possible prize on current attempt: ${maxPrize/prizeCoef[attempt]}$`, '');
+        if (inputNumber === randomNumber) {
+            totalPrize = totalPrize + maxPrize / prizeCoef[attempt];
+            nextGame = confirm(`Congratulation, you won! Your prize is: ${totalPrize}$. Do you want to continue?`);
+            if (nextGame) {
+                attempt = minAttempt;
+                maxPrize *= two;
+                maxNumber += four;
+            } else if (!nextGame) {
+                alert('');
+            }
+        } else {
+            attempt++;
+        }
+    }       
 }
